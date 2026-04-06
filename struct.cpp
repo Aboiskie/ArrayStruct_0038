3cc264f2b07fb17d76a8c8b3fe6f00a6a1593551
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 10:57:54 +0700

    mengisi data

--- struct.cpp
+++ struct.cpp
@@ -12,4 +12,13 @@ int main()
 {
     //deklarasi variabel struct
     mahasiswa mhs;
+    // mengisi  data
+    cout << "isikan data nama :";
+    cin >> mhs.nama;
+    cout << "isikan data alamat :";
+    cin >> mhs.alamat;
+    cout << "isikan data umur :";
+    cin >> mhs.umur;
+
+    
 }
