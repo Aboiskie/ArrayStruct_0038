c4eed355aa06c98ab2c8f75e448a4f563c7f085c
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 10:59:27 +0700

    lanjut ke struct 2

---
+++ struct2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    //deklarasi variabel struct
+    mahasiswa mhs;
+    // mengisi  data
+    cout << "isikan data nama :";
+    cin >> mhs.nama;
+    cout << "isikan data alamat :";
+    cin >> mhs.alamat;
+    cout << "isikan data umur :";
+    cin >> mhs.umur;
+
+    //menampilkan data
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
