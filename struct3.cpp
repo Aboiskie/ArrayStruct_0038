75d1c8a89d5b6e17e63de12438f4e1094902fcf2
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 11:10:39 +0700

    menambahkan include string

---
+++ struct3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string Desa;
+    string Kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
