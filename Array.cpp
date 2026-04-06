061d4823f2c5660b06a6ddbbc584259cf18acce7
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 10:44:46 +0700

    menampilkan data dengan perintah satu persatu

--- Array.cpp
+++ Array.cpp
@@ -13,4 +13,12 @@ int main ()
    // mengisi array
    dataku[1] = 200;
    cout << "isi data kedua setelah diubah : " << dataku[1] << endl;
+   // menampilkan data dengan perintah satu persatu
+   cout << "data pertama = " << datamu[0] << endl;
+   cout << "data kedua = " << datamu[1] << endl;
+   cout << "data ketiga = " << datamu[2] << endl;
+   cout << "data keempat = " << datamu[3] << endl;
+   cout << "data kelima = " << datamu[4] << endl;
+
+   //
 }
