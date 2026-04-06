078cc4ea9ed2e53c9da5ea9c1a0af8af35821a46
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 11:17:13 +0700

    menambahkan variabel mahasiswa menjadi 3

---
+++ struct4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string Desa;
+    string Kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs [3];
+    //Mengisi data
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Isikan Data Nama : ";
+        getline(cin, mhs[i].nama);
+        cout << "Isikan Data Kota : ";
+        getline(cin, mhs[i].alamat.Kota);
+        cout << "Isikan Data Desa : ";
+        getline(cin, mhs[i].alamat.Desa);
+        cout << "Isikan Data Usia : ";
+        cin >> mhs[i].umur;
+        cin.ignore(); // Membersihkan buffer input
+    }
+
+    cout << "Data Mahasiswa" << endl;
+    for(int i = 0; i < 3; i++)
+    {
+        cout << "Nama : " << mhs[i].nama << endl;
+        cout << "Kota : " << mhs[i].alamat.Kota << endl;
+        cout << "Desa : " << mhs[i].alamat.Desa << endl;
+        cout << "Usia : " << mhs[i].umur << endl;
+    }
+}
