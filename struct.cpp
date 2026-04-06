644f7eaabdaa9076eb0726972a8b2033ea900084
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-06 10:58:32 +0700

    menampilkan data

--- struct.cpp
+++ struct.cpp
@@ -20,5 +20,9 @@ int main()
     cout << "isikan data umur :";
     cin >> mhs.umur;
 
-    
+    //menampilkan data
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
